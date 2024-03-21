6eadbc38064abbfef2de6b031e41b2501df57edc
Bahaa202 <bebocode22@gmail.com>
2024-03-21 08:05:41 +0200

    sixth

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+const User = require("../models/user");
 
 
 router.post('/', async (req, res) => {
   try {
     const userId = req.user._id; // Retrieve user ID from token
     // Assuming you store the customer ID retrieved from Stripe in your User model
-    const user = await findById(userId);
+    const user = await User.findById(userId);
     if (!user.stripeCustomerId) return res.json({ message: 'No payment methods found' });
 
     const paymentMethods = await stripe.paymentMethods.list({
